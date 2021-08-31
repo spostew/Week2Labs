0ace3aad4f5916a1467f777dfbe2d68cd7a25441
spostew <spencerunitt0@gmail.com>
2021-08-30 19:37:59 -0700

    Add project files.

---
+++ Lab1/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // print a message to the screen
+            Console.WriteLine("Hello out there.");
+            Console.WriteLine("I will add three numbers for you.");
+            Console.WriteLine("Enter one number and press enter, then a second number and finally, a third number: ");
+
+            // declare two integer variables
+            int n1, n2, n3, sum, product;
+            double divide;
+
+            string userInput;
+            userInput = Console.ReadLine();
+            n1 = Int32.Parse(userInput);
+            n2 = Int32.Parse(Console.ReadLine());
+            n3 = Int32.Parse(Console.ReadLine());
+
+            // calculate the sum of these two numbers
+            sum = n1 + n2 + n3;
+            product = n1 * n2 * n3;
+            divide = product / (double)sum;
+
+            // print a message along with the sum of the two numbers
+            Console.WriteLine("The sum of those three numbers is: " + sum);
+        
+            Console.WriteLine("The product of those three numbers is: " + product);
+            
+            Console.WriteLine("The divsion of the sum / the product is: " + divide);
+           
+            //Just to pause the screen.
+            Console.ReadLine();
+        }
+    }
+}
