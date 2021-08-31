5afac1540fbabdc4db66711f289b7e69981d359f
spostew <spencerunitt0@gmail.com>
2021-08-30 20:32:03 -0700

    Lab2 entered and comments added to each lab

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,4 +1,21 @@
-﻿using System;
+﻿/// Chapter No. 2		Lab No. 1
+/// File Name: Lab1.java
+/// @author: Spencer Unitt
+/// Date:  August 30, 2021
+///
+/// Problem Statement: Ask the user to enter three numbers, calculate the sum of these three numbers.
+/// Then multiply the three numbers used and calculate the product of the three numbers. 
+/// Finally divide the product with the sum and display the results of each equation to the user.
+/// 
+/// Overall Plan:
+/// 1) Print an initial welcoming message to the screen
+/// 2) Prompt the user for three integers
+/// 3) Calculate the sum and product of the integers
+/// 4) Determine the quotient of the product and the sum
+/// 5) Display the results of each equation to the user
+/// 
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,19 +32,23 @@ namespace Lab1
             Console.WriteLine("I will add three numbers for you.");
             Console.WriteLine("Enter one number and press enter, then a second number and finally, a third number: ");
 
-            // declare two integer variables
+            // declare three integer variables, the sum and the product
             int n1, n2, n3, sum, product;
+            // the divide varible will be a double to show nums after the decimal
             double divide;
 
+            // Obtains the nums the user enters
             string userInput;
             userInput = Console.ReadLine();
             n1 = Int32.Parse(userInput);
             n2 = Int32.Parse(Console.ReadLine());
             n3 = Int32.Parse(Console.ReadLine());
 
-            // calculate the sum of these two numbers
+            // calculate the sum of these three numbers
             sum = n1 + n2 + n3;
+            // calculate the product of the three numbers
             product = n1 * n2 * n3;
+            // determine the quotient of the product and sum
             divide = product / (double)sum;
 
             // print a message along with the sum of the two numbers
---
+++ Lab2/Program.cs
@@ -0,0 +1,50 @@
+﻿/// Chapter No. 2		Lab No. 2
+/// File Name: Lab2.java
+/// @author: Spencer Unitt
+/// Date:  August 30, 2021
+///
+/// Problem Statement: Using division and the module operator, determine how many candy bars and gumballs the user can get
+/// with n amount of coupons. Show the amount of each and the remainder of coupons the user will have.
+/// 
+/// 
+/// Overall Plan:
+/// 1) Create 5 different variables that will hold the value of each item you wish to calculate.
+/// 2) Divide the coupons by 10 and store in the number of candy bars variable
+/// 3) Determine the number of coupons left by using the module operater with the number of coupons
+/// 4) Determine the number of gumballs the same way as above.
+/// 5) Display the results of each item to the user in a formated line.
+/// 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Variables used to store the values
+            int num_coupons = 108, num_coupon_after_candybar, num_coupon_after_gumballs, num_candy_bar, num_gumball;
+
+            // Calculate the number of candy bars
+            num_candy_bar = num_coupons / 10;
+
+            // Determines the remainder of the coupons with the module operator
+            num_coupon_after_candybar = num_coupons % 10;
+
+            // Calculate the number of gumballs
+            num_gumball = num_coupon_after_candybar / 3;
+
+            // Determine the remaining total coupons
+            num_coupon_after_gumballs = num_coupon_after_candybar % 3;
+
+            // Displays the results to the user
+            Console.WriteLine("Your {0} coupons can be redeemed for {1} candybars and {2} gumballs with {3} coupons leftover", num_coupons, num_candy_bar, num_gumball, num_coupon_after_gumballs);
+            Console.ReadLine();
+        }
+    }
+}
